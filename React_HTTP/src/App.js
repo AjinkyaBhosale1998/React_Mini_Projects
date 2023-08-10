95c92018fa303109420315c9a8c2820cccc02e4f
Ajinkya Bhosale <ajinkya.bhosale@ascendion.com>
2023-08-11 00:27:57 +0530

    react commit

--- React_HTTP/src/App.js
+++ React_HTTP/src/App.js
@@ -13,12 +13,13 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://react-http-6b4a6.firebaseio.com/movies.json');
+      const response = await fetch('https://dummy-firebase-http-default-rtdb.firebaseio.com/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
+      console.log(data);
 
       const loadedMovies = [];
 
@@ -43,7 +44,7 @@ function App() {
   }, [fetchMoviesHandler]);
 
   async function addMovieHandler(movie) {
-    const response = await fetch('https://react-http-6b4a6.firebaseio.com/movies.json', {
+    const response = await fetch('https://dummy-firebase-http-default-rtdb.firebaseio.com/movies.json', {
       method: 'POST',
       body: JSON.stringify(movie),
       headers: {
